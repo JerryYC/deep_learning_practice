c71824b3bdd84ec7fcb20cb1c12c7d47e8519351
JerryYC <900116jerry@gmail.com>
2021-05-03 19:35:38 -0700

    DNN

---
+++ DNN/model.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+class NeuralNet(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(NeuralNet, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size) 
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, num_classes)  
+    
+    def forward(self, x):
+        out1 = self.fc1(x)
+        out2 = self.relu(out1)
+        out = self.fc2(out2)
+        return out
